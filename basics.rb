ce15aa95cc68831189cf83a3344c1bd6caab173a
Vivek Patel <vnp229@gmail.com>
2016-03-07 12:49:16 -0600

    all lines print true

--- basics.rb
+++ basics.rb
@@ -1,74 +1,74 @@
 print "1. "
-puts 5 == 2 * 2
+puts 4 == 2 * 2
 
 print "2. "
-puts 3 > 4
+puts 5 > 4
 
 print "3. "
-puts (3 * 2) > 10
+puts (6 * 2) > 10
 
 print "4. "
-puts 2 == 3
+puts 3 == 3
 
 print "5. "
-puts (1 > 0) && (2 <= 1) == true
+puts (1 > 0) && (1 <= 1) == true
 
 print "6. "
-puts 2 != 2
+puts 3 != 2
 
 print "7. "
-puts (14 <= 10) || (2 >= 3) == true
+puts (1 <= 10) || (2 >= 3) == true
 
 print "8. "
-puts 10 % 3 == 0
+puts 9 % 3 == 0
 
 print "9. "
-puts 12 == 10 ** 2
+puts 100 == 10 ** 2
 
 print "10. "
-puts 1.6 == 1000 / (300 / 2)
+puts 100/15 == 1000 / (300 / 2)
 
 #I AM A COMMENT RUBY IGNORES ME BECAUSE MY LINE STARTED WITH A '#'
 #Use the following variable msg for questions 11 - 15
 msg = "hEllO"
 
 print "11. "
-puts msg == "HELLO"
+puts msg.upcase == "HELLO"
 
 print "12. "
-puts msg == "HeLLo"
+puts msg.swapcase == "HeLLo"
 
 print "13. "
-puts msg == "OllEh"
+puts msg.reverse == "OllEh"
 
 print "13a. "
-puts msg == "OLLEH"
+puts msg.reverse.upcase == "OLLEH"
 
 arr = ['a', 'b', 'c']
 
 print "14. "
-puts arr == 'c'
+puts arr.last == 'c'
 
 print "15. "
-puts 5 == arr.count
+puts 3 == arr.count
 
 print "16. "
-puts arr == 'a,b,c'
+puts arr.join(',') == 'a,b,c'
 
 #Hint: This will take 2 method calls
 print "17. "
-puts arr == 'C'
+puts arr.last.upcase == 'C'
 
 hsh = { name: 'Fido', age: '99' }
 print "18. "
-puts  hsh == 'Fido'
+puts  hsh[:name] == 'Fido'
 
 print "19. "
-puts hsh == [:name, :age]
+puts hsh.keys == [:name, :age]
 
 print "20. "
-puts hsh == ['Fido', '99']
+puts hsh.values == ['Fido', '99']
 
 
 print "21. "
-puts hsh == { name: 'Fido', age: '99', special: 'Dog' }
+puts hsh.merge({special: 'Dog'}) == { name: 'Fido', age: '99', special: 'Dog' }
